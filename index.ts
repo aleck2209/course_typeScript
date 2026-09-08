9772a3fc3a1c97d8e59c724e79b0feefe06a1c0f
aleck2209 <aleckyann09@gmail.com>
2026-09-08 10:42:46 +0100

    obligatory types basics lesson

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@ const menu = [
 	{ name: "Veggie", price: 9 },
 ];
 
-const cashInRegister = 100;
-const nextOrderId = 1;
+let cashInRegister = 100;
+let nextOrderId = 1;
 const orderQueue = [];
 
 const addNewPizza = (pizzaObj) => {
@@ -15,7 +15,11 @@ const addNewPizza = (pizzaObj) => {
 
 const placeOrder = (pizzaName) => {
 	const selectedPizza = menu.find((item) => item.name === pizzaName);
-	cashInRegister += selectedPizza.name;
+    if(!selectedPizza) {
+        console.error(`${pizzaName} does not exist in the menu`);
+        return;
+    }
+	cashInRegister += selectedPizza.price;
 	const newOrder = {
         id: nextOrderId++,
 		pizza: selectedPizza,
@@ -41,3 +45,10 @@ completeOrder("1")
 console.log("Menu:", menu)
 console.log("Cash in register:", cashInRegister)
 console.log("Order queue:", orderQueue)
+
+let myName: string = "Bob"
+// Primitive Data Types: string, number, boolean
+// Challenge: Explicitly type the variables below:
+
+let numberOfWheels: number = 4
+let isStudent: boolean = false
