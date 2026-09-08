5c198a0db917b0df8eb945f53e3630b7c8793e87
aleck2209 <aleckyann09@gmail.com>
2026-09-08 16:30:55 +0100

    feat: Proposition de solution pour le challenge de la leçon "utility types & partial"

--- src/index.ts
+++ src/index.ts
@@ -87,3 +87,33 @@ export function getPizzaDetail(identifier: string | number): Pizza | undefined {
 console.log("Menu:", menu)
 // console.log("Cash in register:", cashInRegister)
 // console.log("Order queue:", orderQueue)
+
+type User = {
+    id: number
+    username: string
+    role: "member" | "contributor" | "admin"
+}
+
+const users: User[] = [
+    { id: 1, username: "john_doe", role: "member" },
+    { id: 2, username: "jane_smith", role: "contributor" },
+    { id: 3, username: "alice_jones", role: "admin" },
+    { id: 4, username: "charlie_brown", role: "member" },
+];
+
+function updateUser(id: number, updates: any) {
+    // Find the user in the array by the id
+    // Use Object.assign to update the found user in place. 
+    // Check MDN if you need help with using Object.assign
+	const findUser = users.find(item => item.id === id)
+	if (!findUser) {
+		return
+	}
+	return Object.assign(findUser, updates)
+}
+
+// Example updates:
+updateUser(1, { username: "new_john_doe" });
+updateUser(4, { role: "contributor" });
+
+console.log(users)
