b4784bd13345cf39ee3ab1e4e833d48c0d8a9677
aleck2209 <aleckyann09@gmail.com>
2026-09-08 17:23:02 +0100

    feat: réalisation du challenge sur les types génériques de la leçon "Generics"

--- src/index.ts
+++ src/index.ts
@@ -89,3 +89,21 @@ export function getPizzaDetail(identifier: string | number): Pizza | undefined {
 console.log("Menu:", menu)
 // console.log("Cash in register:", cashInRegister)
 // console.log("Order queue:", orderQueue)
+
+const gameScores = [14, 21, 33, 42, 59]
+const favoriteThings = ["raindrops on roses", "whiskers on kittens", "bright copper kettles", "warm woolen mittens"];
+const voters = [{ name: "Alice", age: 42 }, { name: "Bob", age: 77 }]
+
+function getLastItem<PlaceholderType>(array: PlaceholderType[]) {
+    return array[array.length - 1]
+}
+
+/**
+ * Mini-challenge: call `getLastItem` (and console.log the returned value)
+ * on each of the 3 arrays above. Hover over different values to see what the Intellisense
+ * says about the types for each one.
+ */
+
+console.log(getLastItem(gameScores))
+console.log(getLastItem(favoriteThings))
+console.log(getLastItem(voters))
