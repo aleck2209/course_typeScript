3dd297c08aa24694c949496ea41f2624e719a5f0
aleck2209 <aleckyann09@gmail.com>
2026-09-08 17:28:03 +0100

    feat: réalisation du challenge1 de la leçon "Generics"

--- src/index.ts
+++ src/index.ts
@@ -94,7 +94,7 @@ const gameScores = [14, 21, 33, 42, 59]
 const favoriteThings = ["raindrops on roses", "whiskers on kittens", "bright copper kettles", "warm woolen mittens"];
 const voters = [{ name: "Alice", age: 42 }, { name: "Bob", age: 77 }]
 
-function getLastItem<PlaceholderType>(array: PlaceholderType[]) {
+function getLastItem<T>(array: T[]): T {
     return array[array.length - 1]
 }
 
