c632cf3deaac6be28cb719fe4cdc550ff6308ca2
aleck2209 <aleckyann09@gmail.com>
2026-09-25 18:38:50 +0100

    feat: exercice leçon Custom Component Prop Types.

--- typescript-react/src/components/GameStatus.tsx
+++ typescript-react/src/components/GameStatus.tsx
@@ -1,6 +1,15 @@
 import clsx from "clsx";
 import { getFarewellText } from "../utils";
 import { languages } from "../languages";
+import type { JSX } from "react";
+
+type GameStatusProps = {
+	isGameWon: boolean;
+	isGameLost: boolean;
+	isGameOver: boolean;
+	isLastGuessIncorrect: boolean;
+	wrongGuessCount: number;
+};
 
 export default function GameStatus({
 	isGameWon,
@@ -8,8 +17,11 @@ export default function GameStatus({
 	isGameOver,
 	isLastGuessIncorrect,
 	wrongGuessCount,
-}) {
-	const gameStatusClass = clsx("game-status", {
+}: GameStatusProps): JSX.Element {
+	/*
+        CHALLENGE: Type the gameStatusClass variable
+        */
+	const gameStatusClass: string = clsx("game-status", {
 		won: isGameWon,
 		lost: isGameLost,
 		farewell: !isGameOver && isLastGuessIncorrect,
